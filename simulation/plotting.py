4f1c4d2f30d880c11e9033ee30b90b2593f3bed6
Luciano Augusto Campagnoli da Silva <lucianoacsilva@MacBook-Pro-de-Luciano.local>
2020-11-25 21:41:15 -0300

    Corrects some bugs in core.py

--- simulation/plotting.py
+++ simulation/plotting.py
@@ -167,13 +167,13 @@ def print_tips_over_time_multiple_agents_with_tangle(self, no_current_transactio
     tips = self.get_tips()
     for tip in tips:
         # self.DG.node[tip]["node_color"] = '#ffdbb8'
-        self.DG.node[tip]["node_color"] = self.agent_tip_colors[int(str(tip.agent))]
+        self.DG.nodes[tip]["node_color"] = self.agent_tip_colors[int(str(tip.agent))]
 
     #Didn't work on Linux
     # col = list(nx.get_node_attributes(self.DG, 'node_color').values())
     col = []
     for transaction in self.DG:
-        col.append(self.DG.node[transaction]["node_color"])
+        col.append(self.DG.nodes[transaction]["node_color"])
 
     #Creating figure
     #plt.figure(figsize=(12, 6))
